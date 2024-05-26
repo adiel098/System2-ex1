1549e25a5dff195e4ee422fcb936eb1da7361f26
Adiel Halevi <49694400+adiel098@users.noreply.github.com>
2024-05-26 14:15:37 +0300

    Add files via upload

---
+++ Graph.cpp
@@ -0,0 +1,172 @@
+//ID:07988213
+//E-mail: haleviadiel@gmail.com
+
+#include <iostream>
+#include <vector>
+#include "Graph.hpp"
+
+ namespace ariel{
+
+        // Default constructor
+        Vertex::Vertex() 
+        {}
+
+
+        Vertex::Vertex(int data)
+        {this->data =data;}
+
+
+     int Vertex::get_data() const
+    {
+        return data;
+    }
+
+
+        std::vector<Edge>& Vertex::get_Neighbours()
+        {
+            return this->neighbours;
+        }
+
+        // Default constructor
+        Edge::Edge()
+        {}
+        
+        Edge::Edge(Vertex& dest,int weight)
+        {
+            this->dest = &dest;
+            this-> weight = weight;
+        }
+
+
+        int Edge::get_weight()
+        {
+            return weight;
+        }
+         Vertex& Edge::get_dest()
+        {
+            return *dest;
+        }   
+        // Default constructor
+        Graph::Graph()
+        { }
+
+        bool Graph::get_isWeighted()
+        {
+            return isWeighted;
+        }
+
+        Vertex& Graph::get_vertex(int data)
+        {
+            if (data >= 0 && data < vertices.size()) 
+            {
+                return vertices[data];
+            } 
+            else 
+            {  
+                throw std::out_of_range("Index out of range");
+                // or return nullptr;
+            }
+        }
+        std::vector<Vertex>Graph::get_graphVertices()
+        {
+            return vertices;
+        }
+        bool Graph::get_graphDirected()
+        {
+            return isDirected;
+        }
+
+        void Graph::set_graphDirected(bool flag)
+        {
+            this->isDirected = flag;
+        }
+        int Graph::get_countVertices()
+        {
+            return countVertices;
+        }
+
+        void Graph::set_isWeighted(bool flag)
+        {
+            this->isWeighted = flag;
+        }
+
+        void Graph::add_Neighbour(Vertex& source,Vertex& destination,int weight)
+        {
+            if(source.get_data() < vertices.size())
+            {
+                Edge* e =  new Edge(destination,weight);
+                vertices[source.get_data()].get_Neighbours().push_back(*e);
+                countEdges++;
+
+            }
+            else
+            {
+                throw std::out_of_range("Source index out of range");
+            }
+            
+        }
+
+        void Graph::add_Vertex(Vertex v)
+        {
+            vertices.push_back(v);
+            countVertices++;
+
+        }
+        void Graph::printGraph()
+        {
+
+            std:: cout << "Graph with " << countVertices  << " vertices and " <<  countEdges  << " edges \n"; 
+        }
+
+        void Graph::loadGraph(const std::vector<std::vector<int>>& adj_Mat)
+        {
+            countVertices = 0;
+            countEdges = 0;
+            isWeighted = false;
+            vertices.clear(); 
+            if(adj_Mat.size() != adj_Mat[0].size())
+            {
+                throw std::invalid_argument("Invalid graph: The graph is not a square matrix.\n");
+            }
+
+            // Use 'this' pointer to access the current object
+            for (size_t i = 0; i < adj_Mat.size(); i++)
+            {
+                Vertex* v = new Vertex(i);
+                this->add_Vertex(*v);
+            }
+
+            for (size_t j = 0; j < adj_Mat.size(); j++)
+            {
+                Vertex& source = this->get_vertex(j);
+                for (size_t k = 0; k < adj_Mat.size(); k++)
+                {
+                    if (adj_Mat[j][k] > 1)
+                    {
+                        this->set_isWeighted(true);
+                    }
+
+                    if(adj_Mat[j][k] !=adj_Mat[k][j])
+                    {
+                        set_graphDirected(true);
+                    } 
+                    if(adj_Mat[j][k] >= 1)
+                    {
+                         Vertex& dest = this->get_vertex(k);
+                         this->add_Neighbour(source, dest, adj_Mat[j][k]);
+                    }
+                   
+                }
+            }
+        }
+        
+
+
+ }
+
+
+
+
+
+
+
---
+++ Test.cpp
@@ -0,0 +1,209 @@
+//ID:07988213
+//E-mail: haleviadiel@gmail.com
+
+#include "doctest.h"
+#include "Algorithms.hpp"
+#include "Graph.hpp"
+#include <vector>
+#include <stdexcept>
+
+
+using namespace std;
+using namespace ariel;
+
+
+
+TEST_CASE("Test isConnected")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isConnected(g) == "1");
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isConnected(g) == "0");
+}
+
+TEST_CASE("Test shortestPath")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 2) == "0->1->2");
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 4) == "-1");
+}
+
+TEST_CASE("Test isContainsCycle")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == "0");
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isContainsCycle(g) == "The cycle is: 0->1->2->0");
+}
+
+TEST_CASE("Test isBipartite")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::isBipartite(g) == "The graph is bipartite: A={0, 2}, B={1}");
+
+    vector<vector<int>> graph2 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+    g.loadGraph(graph2);
+    CHECK(ariel::Algorithms::isBipartite(g) == "0");
+
+    vector<vector<int>> graph3 = {
+        {0, 1, 2, 0, 0},
+        {1, 0, 3, 0, 0},
+        {2, 3, 0, 4, 0},
+        {0, 0, 4, 0, 5},
+        {0, 0, 0, 5, 0}};
+    g.loadGraph(graph3);
+    CHECK(ariel::Algorithms::isBipartite(g) == "0");
+}
+TEST_CASE("Test invalid graph")
+{
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 2, 0},
+        {1, 0, 3, 0},
+        {2, 3, 0, 4},
+        {0, 0, 4, 0},
+        {0, 0, 0, 5}};
+    CHECK_THROWS(g.loadGraph(graph));
+}
+TEST_CASE("Test isConnected with additional scenarios") {
+    // Test a connected graph with more vertices
+    ariel::Graph g;
+    vector<vector<int>> connectedGraph = {
+        {0, 1, 0, 0, 1},
+        {1, 0, 1, 0, 0},
+        {0, 1, 0, 1, 0},
+        {0, 0, 1, 0, 1},
+        {1, 0, 0, 1, 0}};
+    g.loadGraph(connectedGraph);
+    CHECK(ariel::Algorithms::isConnected(g) == "1");
+
+    // Test an empty graph
+    ariel::Graph emptyGraph;
+    CHECK(ariel::Algorithms::isConnected(emptyGraph) == "1");
+
+    // Test a graph with only one vertex
+    ariel::Graph singleVertexGraph;
+    singleVertexGraph.add_Vertex(Vertex(0));
+    CHECK(ariel::Algorithms::isConnected(singleVertexGraph) == "1");
+}
+
+TEST_CASE("Test shortestPath with additional scenarios") {
+    // Test a graph with multiple shortest paths between vertices
+    ariel::Graph g;
+    vector<vector<int>> graph = {
+        {0, 1, 1, 0},
+        {1, 0, 1, 1},
+        {1, 1, 0, 1},
+        {0, 1, 1, 0}};
+    g.loadGraph(graph);
+    CHECK(ariel::Algorithms::shortestPath(g, 0, 3) == "0->1->3");
+
+    // Test a graph with no path between vertices
+    ariel::Graph noPathGraph;
+    vector<vector<int>> noPathMatrix = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}};
+    noPathGraph.loadGraph(noPathMatrix);
+    CHECK(ariel::Algorithms::shortestPath(noPathGraph, 0, 2) == "-1");
+
+    // Test a graph with negative weights
+    ariel::Graph negativeWeightGraph;
+    vector<vector<int>> negativeWeights = {
+        {0, 1, -1},
+        {1, 0, 2},
+        {-1, 2, 0}};
+    negativeWeightGraph.loadGraph(negativeWeights);
+    CHECK(ariel::Algorithms::shortestPath(negativeWeightGraph, 0, 2) == "0->1->2");
+}
+
+TEST_CASE("Test isContainsCycle with additional scenarios") {
+    // Test a graph without a cycle
+    ariel::Graph acyclicGraph;
+    vector<vector<int>> acyclicMatrix = {
+        {0, 1, 0, 0},
+        {1, 0, 1, 0},
+        {0, 1, 0, 1},
+        {0, 0, 1, 0}};
+    acyclicGraph.loadGraph(acyclicMatrix);
+    CHECK(ariel::Algorithms::isContainsCycle(acyclicGraph) == "0");
+
+    // Test a cyclic graph with multiple cycles
+    ariel::Graph cyclicGraph;
+    vector<vector<int>> cyclicMatrix = {
+        {0, 1, 1, 0},
+        {1, 0, 1, 0},
+        {1, 1, 0, 1},
+        {0, 0, 1, 0}};
+    cyclicGraph.loadGraph(cyclicMatrix);
+    CHECK(ariel::Algorithms::isContainsCycle(cyclicGraph) == "The cycle is: 0->1->2->0");
+
+    // Test a graph with a self-loop
+    ariel::Graph selfLoopGraph;
+    vector<vector<int>> selfLoopMatrix = {
+        {0, 1, 0, 0},
+        {1, 0, 0, 0},
+        {0, 0, 0, 1},
+        {0, 0, 1, 0}};
+    selfLoopGraph.loadGraph(selfLoopMatrix);
+    CHECK(ariel::Algorithms::isContainsCycle(selfLoopGraph) == "0");
+}
+
+
+TEST_CASE("Test invalid graph with additional scenarios") {
+    // Test a non-square graph
+    ariel::Graph nonSquareGraph;
+    vector<vector<int>> nonSquareMatrix = {
+        {0, 1, 0},
+        {1, 0, 1}};
+    CHECK_THROWS(nonSquareGraph.loadGraph(nonSquareMatrix));
+
+    
+}
