30c77641651f1640fd0cd269d376ab8a4519c0e3
Adiel Halevi <49694400+adiel098@users.noreply.github.com>
2024-06-03 13:52:01 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -8,63 +8,25 @@
 namespace ariel
 {
 
-    class Edge;
-    class Vertex;
     class Graph;
-
-
-class Vertex {
-    
-    private:
-        std::vector<Edge> neighbours;   
-        int data;
-
-    public:
-        Vertex();
-        Vertex(int data);
-        int get_data() const;
-        std::vector<Edge>& get_Neighbours();
-      
-
-};
-
-class Edge{
-
-    private:
-        Vertex* dest;
-        int weight;
-
-    public:
-        Edge();
-        Edge(Vertex& dest,int weight);
-        Vertex& get_dest();
-        int get_weight();
-
-};
 
 class Graph {
     // Class members and methods
     private:
         bool isDirected=false;
         bool isWeighted=false;  
-
         int countVertices =0;
         int countEdges =0;
-        std::vector<Vertex> vertices;
+        std::vector<std::vector<int>> adj_Mat;
+
 
     public:
         Graph();
-        std::vector<Vertex>  get_graphVertices();
-        Vertex& get_vertex(int data);
-
         int get_countVertices();
-        bool get_graphDirected();
-        void set_graphDirected(bool flag);
+        bool get_graphDirected()const;
+        std::vector<std::vector<int>> get_matrix()const ;
 
-        void set_isWeighted(bool flag);
-        bool get_isWeighted();
-        void add_Vertex(Vertex v);
-        void add_Neighbour(Vertex& source,Vertex& dest,int weight);
+        bool get_isWeighted()const;
         void loadGraph(const std::vector<std::vector<int>>& adj_Mat);
         void printGraph();
 
